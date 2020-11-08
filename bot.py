64dee50a8be31c7dbc8a76e6fe9e3605c7df4385
Heytor Victor <heytorvas@gmail.com>
2020-11-08 16:25:52 -0300

    update

--- bot.py
+++ bot.py
@@ -4,33 +4,21 @@ def get_credentials():
     with open('config.json') as config_file:
         return json.load(config_file)
 
-def create_api(credentials):
-    auth = tweepy.OAuthHandler(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
-    auth.set_access_token(credentials["ACCESS_TOKEN"], credentials["ACCESS_TOKEN_SECRET"])
-
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    return api
-
 credentials = get_credentials()
-api = create_api(credentials)
+
+auth = tweepy.OAuthHandler(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
+auth.set_access_token(credentials["ACCESS_TOKEN"], credentials["ACCESS_TOKEN_SECRET"])
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
 search = 'fome'
-number_tweets = 10000
+number_tweets = 10
 
 for tweet in tweepy.Cursor(api.search, q=search).items(number_tweets):
     if not tweet.favorited:
         try:
             tweet.favorite()
-            print('Tweet favorited')
-        except Exception as e:
-            print('Error on favorite')
+            print('Tweet liked')
+        except tweepy.TweepError as e:
+            print('Error on like')
     else:
         print('Tweet already favorited')
-
-    # if not tweet.retweeted:
-    #     try:
-    #         tweet.retweet()
-    #         print('Tweet retweeted')
-    #     except Exception as e:
-    #         print("Error on retweet")
-    # else:
-    #     print('Tweet already retweeted')
