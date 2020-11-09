f95da4fa85cd84170fdd0ce29168729d05f89f24
Heytor Víctor <heytorvas@gmail.com>
2020-11-09 15:48:50 -0300

    update final loop

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import tweepy, json
 from time import sleep
+from datetime import datetime
 from translate import Translate
 
 def get_credentials():
@@ -12,24 +13,61 @@ def create_api(credentials):
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     return api
 
+def twitter_rates():
+    returned = 0
+    stats = api.rate_limit_status()  #stats['resources'].keys()
+    for akey in stats['resources'].keys():
+        if type(stats['resources'][akey]) == dict:
+            for anotherkey in stats['resources'][akey].keys():
+                if type(stats['resources'][akey][anotherkey]) == dict:
+                    #print(akey, anotherkey, stats['resources'][akey][anotherkey])
+                    limit = (stats['resources'][akey][anotherkey]['limit'])
+                    remaining = (stats['resources'][akey][anotherkey]['remaining'])
+                    used = limit - remaining
+                    if used != 0:
+                        print("Twitter API used", used, "remaining queries", remaining,"for query type", anotherkey)
+                        returned = int(remaining)
+                    else:
+                        pass
+                else:
+                    pass  #print("Passing")  #stats['resources'][akey]
+        else:
+            print(akey, stats['resources'][akey])
+            print(stats['resources'][akey].keys())
+            limit = (stats['resources'][akey]['limit'])
+            remaining = (stats['resources'][akey]['remaining'])
+            used = limit - remaining
+            if used != 0:
+                print("Twitter API:", used, "requests used,", remaining, "remaining, for API queries to", akey)
+                returned = int(remaining)
+                pass
+    
+    return returned
+
+
 t = Translate()
 search = t.read_txt_file()
 number_tweets = 8
 count = 0
 print('Words done')
 
-while True:
+api = create_api(get_credentials())
+remaining = twitter_rates()
 
-    if count > 170:
-        print('Sleep')
-        sleep(60*15)
-        count = 0
+while True:
 
     api = create_api(get_credentials())
     print('API done')
 
     for word in search:
         for tweet in tweepy.Cursor(api.search, q=word).items(number_tweets):
+            if count == remaining:
+                print('Sleep')
+                sleep(60*15)
+                count = 0
+                api = create_api(get_credentials())
+                print('API done')
+
             try:
                 tweet.favorite()
                 print('Tweet liked')
